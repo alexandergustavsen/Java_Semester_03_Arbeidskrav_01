f8b7622436aed6fe9677985b1561edff9a3d19bb
alexandergustavsen <alexander_gustavsen@hotmail.com>
2018-09-26 14:34:15 +0200

    new

--- src/main/HttpPath.java
+++ src/main/HttpPath.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class HttpPath {
+
+    private String path;
+
+    public HttpPath(String path) {
+        this.path = path;
+    }
+
+    public HttpQuery getQuery() {
+        int questionPos = path.indexOf("?");
+        String query = path.substring(questionPos+1);
+        return new HttpQuery(query);
+    }
 }
